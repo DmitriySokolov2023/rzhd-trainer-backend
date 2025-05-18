3fcb304b6a1d56e69c96a21d289ff66c6769e626
dmitriysokolov2023 <sokolovoff2023@gmail.com>
2025-05-18 23:07:49 +0300

    add-serv

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import 'colors'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
@@ -17,35 +16,45 @@ dotenv.config()
 
 const app = express()
 const __dirname = path.resolve()
+console.log(__dirname)
 
 async function main() {
 	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
-	app.use(
-		cors({
-			origin: 'http://localhost:5173',
-			credentials: true
-		})
-	)
+	// app.use(
+	// 	cors({
+	// 		origin: 'http://109.184.79.10:5000', // в проде — укажи домен
+	// 		credentials: true
+	// 	})
+	// )
+	// app.use(cors({ origin: true, credentials: true }))
 	app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 	app.use('/api/task', tasksRoutes)
 	app.use('/api/auth', authRoutes)
 	app.use('/api/status', statusRoutes)
 	app.use('/api/user', userRoutes)
 
+	// 👇 Фронтенд (React build)
+	app.use(
+		express.static(path.join(__dirname, '..', 'rzhd-trainer-frontend', 'dist'))
+	) // или build
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.join(__dirname, '..', 'rzhd-trainer-frontend', 'dist', 'index.html')
+		) // или build
+	})
+
 	app.use(notFound)
 	app.use(errorHandler)
 
 	const PORT = process.env.PORT || 5000
-
-	app.listen(
-		PORT,
+	app.listen(PORT, '0.0.0.0', () => {
 		console.log(
 			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
 				.bold
 		)
-	)
+	})
 }
 
 main()
